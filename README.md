03e26e531c20e54314f033f5b77b7262ef39bc48
atuffa <tuffajr@gmail.com>
2018-05-15 14:55:36 -0500

    final app for submission

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # FriendFinder
-A dating app
+This is a friend finder app
+
+A Full stack project that combines Node.js &amp; express.js framework (for routing)
+
+Project built by HTML5, CSS3, Bootsrap 3, javascript(ES6), express.js framwork and node.js 
+-----------------------------------------------------------------------------------------
+
+This app basically is a dating app.
+
+The user is required to input his name and picture link. 
+
+In addtion to his info there are some 10 questions asked which are used to help find a match for the user.
+
+The app is well documented full stack app which is helpful to understand the listed programs.
+
+It is also deployed on heroku.
+
+
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,83 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on friends array etc.
+// ===============================================================================
+
+let friendArray = require("../data/friend.js");
+let path = require("path")
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/friends... they are shown a JSON of the data in the friends)
+  // ---------------------------------------------------------------------------
+
+    app.get("/api/friends", function(req, res) {
+        res.json(friendArray);
+    });
+
+    let sample = [4,3,6,9,6,7,2,1,6,4]
+    app.post("/api/friends", function(req, res){
+        // the new post value inputed
+        let newFriend = req.body;
+        console.log(newFriend);
+
+        // used to store the best match from the databse 
+        let bestMatch = [];
+        
+        // varibales to store the diffrence between the sscore values
+        let rawDiff =[]
+        let totalDiff = 0;
+        let diff = 0;
+
+        // for loop to calculate the diff of score between the user and the list of frieds in the database
+        for (i in friendArray){
+            //   console.log(friendArray[0].scores[0])
+            for(j in friendArray[i].scores){
+                diff = Math.abs(parseInt(friendArray[i].scores[j]) - parseInt(newFriend.scores[j]))
+                totalDiff += diff
+                
+            }//End of for (j)
+
+            // push the totalDiff into the array rawdata
+            rawDiff.push(totalDiff);
+            // console.log(totalDiff)
+
+            // Setting the totalDiff to zero once the i iterantion(loops) is completed
+            totalDiff=0;
+
+        }//End of for (i)
+
+        // get the min value of all the diff's
+        let minValue = Math.min.apply(null, rawDiff)
+        console.log(minValue);
+        
+        // function to find index of if there are multiple element with the same minValue
+        let indexOfMinValue = function(array,element){
+            var counts = [];
+            for (let i in array){
+                if (array[i] === element) {  
+                    counts.push(i);
+                }// End of if condition
+            }//End of for loop
+            return counts;
+        }// End of indexOfMinValue
+
+        // For loop used to push the elements that mactch to bestMatch array
+        for (i in indexOfMinValue(rawDiff, minValue)){
+            bestMatch.push(friendArray[i]);
+        }//End of for loop
+
+        // push the new player to friendArray once the best match is figured out
+        friendArray.push(newFriend);
+
+        // return the best match friend
+        res.json(bestMatch);
+
+    }); // End of app.post
+}//End of module.export
